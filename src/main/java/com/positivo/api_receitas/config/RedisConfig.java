c57bf0d76ba01817548dc5d8204ed73987d0402c
Giovanni Policene <giovannicpoli@gmail.com>
2025-04-24 14:12:12 -0300

    feat: Redis e Cache

---
+++ src/main/java/com/positivo/api_receitas/config/RedisConfig.java
@@ -0,0 +1,25 @@
+package com.positivo.api_receitas.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+
+import java.time.Duration;
+
+import static org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer;
+
+@EnableCaching
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisCacheConfiguration redisCacheConfiguration() {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofMinutes(10))
+                .disableCachingNullValues()
+                .serializeValuesWith(fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+    }
+}
--- src/main/java/com/positivo/api_receitas/services/ReceitaService.java
+++ src/main/java/com/positivo/api_receitas/services/ReceitaService.java
@@ -3,6 +3,7 @@ package com.positivo.api_receitas.services;
 import com.positivo.api_receitas.dto.IngredientesRequestDTO;
 import com.positivo.api_receitas.entities.Receita;
 import com.positivo.api_receitas.repository.ReceitaRepository;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,14 +18,17 @@ public class ReceitaService {
         this.repository = repository;
     }
 
+    @Cacheable(value = "receitas_todas")
     public List<Receita> getAll(){
         return repository.findAll();
     }
 
+    @Cacheable(value = "receitas_por_id", key = "#id")
     public Optional<Receita> getById (Long id) {
         return repository.findById(id);
     }
 
+    @Cacheable(value = "receitas_por_ingredientes", key = "#ingredientes")
     public List<Receita> buscarPorIngredientes(List<String> ingredientes) {
         // Tem que transformar a List em um array porque o JPA não consegue
         // fazer a conversão de List para text[] no PostgreSQL.
